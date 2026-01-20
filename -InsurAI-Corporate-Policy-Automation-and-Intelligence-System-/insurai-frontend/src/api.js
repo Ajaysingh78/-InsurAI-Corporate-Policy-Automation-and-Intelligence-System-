777da7115a71bf601000bd4826d1d3bf22ebac1f
Ajaysingh78 <166209478+Ajaysingh78@users.noreply.github.com>
2026-01-21 02:56:46 +0530

    env update

--- -InsurAI-Corporate-Policy-Automation-and-Intelligence-System-/insurai-frontend/src/api.js
+++ -InsurAI-Corporate-Policy-Automation-and-Intelligence-System-/insurai-frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // API Configuration
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 const API = axios.create({
   baseURL: BASE_URL,
